11653ccd641dcd09f866e66858bb6ec7708c11c7
Fabio Baltieri <fabio.baltieri@gmail.com>
2019-10-27 17:28:58 +0000

    fw: add power board ping mapping

--- firmware/config/custom_board.h
+++ firmware/config/custom_board.h
@@ -1,17 +1,44 @@
-#define LED_1          17 // 31
-#define LED_2          18 // 30
+#if 1
+#define LED_1          7
+#define LED_2          8
 
 #define LEDS_LIST { LED_1, LED_2 }
 #define LEDS_NUMBER    2
 #define LEDS_ACTIVE_STATE 0
 
-#define POWER_LED_1          19 // 31
-#define POWER_LED_2          20 // 30
-#define POWER_LED_3          22 // 29
-#define POWER_LED_4          23 // 28
+#define POWER_LED_1          28
+#define POWER_LED_2          29
+#define POWER_LED_3          30
+#define POWER_LED_4          31
 
-#define BUTTON_1       13 // 5
-#define BUTTON_2       14 // 6
+#define BUTTON_1       5
+#define BUTTON_2       6
+
+#define BSP_BUTTON_0   BUTTON_1
+#define BSP_BUTTON_1   BUTTON_2
+
+#define BUTTONS_NUMBER 2
+#define BUTTON_PULL    NRF_GPIO_PIN_PULLUP
+#define BUTTONS_ACTIVE_STATE 0
+#define BUTTONS_LIST { BUTTON_1, BUTTON_2 }
+
+#define TWI_SCL        27
+#define TWI_SDA        26
+#else /* PCA10040 pins */
+#define LED_1          17
+#define LED_2          18
+
+#define LEDS_LIST { LED_1, LED_2 }
+#define LEDS_NUMBER    2
+#define LEDS_ACTIVE_STATE 0
+
+#define POWER_LED_1          19
+#define POWER_LED_2          20
+#define POWER_LED_3          22
+#define POWER_LED_4          23
+
+#define BUTTON_1       13
+#define BUTTON_2       14
 
 #define BSP_BUTTON_0   BUTTON_1
 #define BSP_BUTTON_1   BUTTON_2
@@ -23,3 +50,4 @@
 
 #define TWI_SCL        24
 #define TWI_SDA        25
+#endif
