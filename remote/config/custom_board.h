cbb476e3680c77b55a7a37b43a8de44c9d38b5d8
Fabio Baltieri <fabio.baltieri@gmail.com>
2019-10-27 17:27:00 +0000

    remote: initial firmware

---
+++ remote/config/custom_board.h
@@ -0,0 +1,19 @@
+#define LED_1          17
+#define LED_2          18
+
+#define LEDS_LIST { LED_1, LED_2 }
+#define LEDS_NUMBER    2
+#define LEDS_ACTIVE_STATE 0
+
+#define BUTTON_1       13
+#define BUTTON_2       14
+#define BUTTON_3       15
+
+#define BSP_BUTTON_0   BUTTON_1
+#define BSP_BUTTON_1   BUTTON_2
+#define BSP_BUTTON_2   BUTTON_3
+
+#define BUTTONS_NUMBER 3
+#define BUTTON_PULL    NRF_GPIO_PIN_PULLUP
+#define BUTTONS_ACTIVE_STATE 0
+#define BUTTONS_LIST { BUTTON_1, BUTTON_2, BUTTON_3 }
