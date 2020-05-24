2079652fd27746dd8fb89968f037fec70bd90c7a
Fabio Baltieri <fabio.baltieri@gmail.com>
2020-05-25 00:36:29 +0100

    bl: clean error functions, use only one led

--- bootloader/main.c
+++ bootloader/main.c
@@ -9,50 +9,25 @@
 #include "nrf_log_ctrl.h"
 #include "nrf_log_default_backends.h"
 #include "app_error.h"
-#include "app_error_weak.h"
 #include "nrf_bootloader_info.h"
 #include "nrf_delay.h"
-
-static void on_error(void)
-{
-	NRF_LOG_FINAL_FLUSH();
-	NVIC_SystemReset();
-}
-
-void app_error_handler(uint32_t error_code, uint32_t line_num, const uint8_t * p_file_name)
-{
-	NRF_LOG_ERROR("%s:%d", p_file_name, line_num);
-	on_error();
-}
-
-void app_error_fault_handler(uint32_t id, uint32_t pc, uint32_t info)
-{
-	NRF_LOG_ERROR("Received a fault! id: 0x%08x, pc: 0x%08x, info: 0x%08x", id, pc, info);
-	on_error();
-}
-
-void app_error_handler_bare(uint32_t error_code)
-{
-	NRF_LOG_ERROR("Received an error: 0x%08x!", error_code);
-	on_error();
-}
 
 static void dfu_observer(nrf_dfu_evt_type_t evt_type)
 {
 	switch (evt_type) {
-		case NRF_DFU_EVT_DFU_FAILED:
-		case NRF_DFU_EVT_DFU_ABORTED:
 		case NRF_DFU_EVT_DFU_INITIALIZED:
 			bsp_board_init(BSP_INIT_LEDS);
 			bsp_board_led_on(BSP_BOARD_LED_0);
-			bsp_board_led_on(BSP_BOARD_LED_1);
-			bsp_board_led_off(BSP_BOARD_LED_2);
 			break;
 		case NRF_DFU_EVT_TRANSPORT_ACTIVATED:
-			bsp_board_led_off(BSP_BOARD_LED_1);
-			bsp_board_led_on(BSP_BOARD_LED_2);
+			bsp_board_led_invert(BSP_BOARD_LED_0);
+			nrf_delay_ms(100);
+			bsp_board_led_invert(BSP_BOARD_LED_0);
 			break;
-		case NRF_DFU_EVT_DFU_STARTED:
+		case NRF_DFU_EVT_OBJECT_RECEIVED:
+			bsp_board_led_invert(BSP_BOARD_LED_0);
+			nrf_delay_ms(33);
+			bsp_board_led_invert(BSP_BOARD_LED_0);
 			break;
 		default:
 			break;
