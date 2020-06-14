e6c79ac381ba5e9faa0b8159157765884bf1caea
Fabio Baltieri <fabio.baltieri@gmail.com>
2020-06-15 00:08:33 +0100

    fw: add auto-shutdown after 15 minutes

--- firmware/system.c
+++ firmware/system.c
@@ -6,6 +6,7 @@
 #include "nrf_drv_twi.h"
 #include "nrf_drv_twi.h"
 #include "nrf_log.h"
+#include "nrf_pwr_mgmt.h"
 
 #include "mic280.h"
 #include "state.h"
@@ -20,9 +21,36 @@ static int8_t temps[] = {INT8_MIN, INT8_MIN};
 #define BATT_NUM (13200 / 2 / 2) // 1.2 * 11 * 1000
 #define BATT_DEN (1024 / 2 / 2) // 10bit
 
+#define SHUTDOWN_DELAY (60 * 15)
+
 APP_TIMER_DEF(sys_tmr);
 static nrf_saadc_value_t adc_buf;
 static const nrf_drv_twi_t twi = NRF_DRV_TWI_INSTANCE(0);
+
+static void maybe_shutdown(void)
+{
+        static uint16_t shutdown_counter = SHUTDOWN_DELAY;
+        ret_code_t err_code;
+
+        if (state.mode != MODE_STANDBY) {
+                shutdown_counter = SHUTDOWN_DELAY;
+                return;
+        }
+
+        NRF_LOG_INFO("shutdown_counter: %d", shutdown_counter);
+        if (shutdown_counter--)
+               return;
+
+        NRF_LOG_INFO("initiate shutdown");
+
+        err_code = app_timer_stop_all();
+        APP_ERROR_CHECK(err_code);
+
+	bsp_board_led_off(0);
+	bsp_board_led_off(1);
+
+        nrf_pwr_mgmt_shutdown(NRF_PWR_MGMT_SHUTDOWN_GOTO_SYSOFF);
+}
 
 static int8_t get_max_temp(void)
 {
@@ -61,6 +89,8 @@ static void timer_handler(void *context)
 {
 	uint8_t i;
 	int32_t temp;
+
+	maybe_shutdown();
 
 	// Update the battery voltage (for the next sample)
 	saadc_convert();
