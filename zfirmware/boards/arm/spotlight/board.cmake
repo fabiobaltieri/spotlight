4b6a8b151b54848b4746ccb7a189bee2ecfe189b
Fabio Baltieri <fabio.baltieri@gmail.com>
2022-07-08 01:10:47 +0100

    zfw: add the initial Zephyr firmware skeleton

---
+++ zfirmware/boards/arm/spotlight/board.cmake
@@ -0,0 +1,5 @@
+board_runner_args(nrfjprog "--softreset")
+board_runner_args(jlink "--device=nrf52" "--speed=4000")
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
