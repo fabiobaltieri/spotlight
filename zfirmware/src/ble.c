bc4e94e0c35c68dba9a11ed8b76a9bf69635770c
Fabio Baltieri <fabio.baltieri@gmail.com>
2022-07-27 15:24:12 +0100

    zfw: add full name in the device ad

--- zfirmware/src/ble.c
+++ zfirmware/src/ble.c
@@ -19,10 +19,12 @@ LOG_MODULE_REGISTER(ble);
 #include "input.h"
 #include "state.h"
 
+#define DEVICE_NAME     CONFIG_BT_DEVICE_NAME
+#define DEVICE_NAME_LEN (sizeof(DEVICE_NAME) - 1)
+
 static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
-	BT_DATA_BYTES(BT_DATA_UUID16_ALL,
-		      BT_UUID_16_ENCODE(BT_UUID_BAS_VAL))
+	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
 };
 
 static uint8_t sl_status[8];
@@ -130,7 +132,7 @@ static int ble_setup(const struct device *arg)
 	os_mgmt_register_group();
 	smp_bt_register();
 
-	err = bt_le_adv_start(BT_LE_ADV_CONN_NAME, ad, ARRAY_SIZE(ad), NULL, 0);
+	err = bt_le_adv_start(BT_LE_ADV_CONN, ad, ARRAY_SIZE(ad), NULL, 0);
 	if (err) {
 		LOG_ERR("Advertising failed to start (err %d)", err);
 		return 0;
