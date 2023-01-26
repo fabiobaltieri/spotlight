9e2b362d3c3d37acf5217f39207bda9b1700497d
Fabio Baltieri <fabio.baltieri@gmail.com>
2023-01-26 19:53:52 +0000

    fw: ble config update, drop manual mcumgr init

--- zfirmware/src/ble.c
+++ zfirmware/src/ble.c
@@ -125,8 +125,6 @@ static int ble_setup(const struct device *arg)
 		return 0;
 	}
 
-	smp_bt_start();
-
 	err = bt_le_adv_start(BT_LE_ADV_CONN, ad, ARRAY_SIZE(ad), NULL, 0);
 	if (err) {
 		LOG_ERR("Advertising failed to start (err %d)", err);
